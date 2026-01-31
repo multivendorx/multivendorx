499533e99d6a74edee437734eda5c9a30fbe8366
buddhadebroy <roybuddhadeb2003@gmail.com>
2026-01-31 20:05:00 +0530

    store page

--- plugins/multivendorx/src/blocks/store-products/productList.tsx
+++ plugins/multivendorx/src/blocks/store-products/productList.tsx
@@ -19,6 +19,7 @@ const ProductList: React.FC<ProductListProps> = ({
 }) => {
 	const [products, setProducts] = useState<Product[]>([]);
 	const [loading, setLoading] = useState(true);
+	const [activeTab, setActiveTab] = useState<'products' | 'store'>('products');
 
 	useEffect(() => {
 		if (isEditor) {
@@ -60,57 +61,74 @@ const ProductList: React.FC<ProductListProps> = ({
 	return (
 		<div className="woocommerce">
 			<div className="product">
+				{/* WooCommerce Tabs */}
 				<div className="woocommerce-tabs wc-tabs-wrapper">
-					{/* tab */}
-					<ul className="tabs wc-tabs">
-						<li>Products</li>
-						<li>Store</li>
+					<ul className="tabs wc-tabs" role="tablist">
+						<li className={`${activeTab === 'products' ? 'active' : ''}`} id="tab-title-products">
+							<a href="#tab-products" onClick={(e) => { e.preventDefault(); setActiveTab('products'); }}>
+								Products
+							</a>
+						</li>
+						<li className={`${activeTab === 'store' ? 'active' : ''}`} id="tab-title-store">
+							<a href="#tab-store" onClick={(e) => { e.preventDefault(); setActiveTab('store'); }}>
+								Store
+							</a>
+						</li>
 					</ul>
 
-					{/* product list */}
-					<ul className="is-flex-container columns-4 wc-block-product-template wp-block-woocommerce-product-template is-layout-flow wp-block-woocommerce-product-template-is-layout-flow">
-						{products.map((product) => (
-							<li className="wc-block-product product type-product status-publish first instock shipping-taxable purchasable product-type-simple">
-								<div className="wc-block-components-product-image wc-block-grid__product-image wc-block-components-product-image--aspect-ratio-auto wp-block-woocommerce-product-image">
-									<a
-										key={product.id}
-										href={product.permalink}
-										className="product-card"
-									>
-										{product.images?.[0]?.src ? (
-											<img
-												src={product.images[0].src}
-												alt={product.name}
-											/>
-										) : (
-											<img src="http://localhost:8889/wp-content/uploads/woocommerce-placeholder.webp" />
-										)}
-									</a>
+					{/* Products Tab Content */}
+					<div className="panel entry-content wc-tab" id="tab-products" style={{ display: activeTab === 'products' ? 'block' : 'none' }}>
+						<ul className="products columns-4">
+							{products.map((product) => (
+								<li className="wc-block-product product type-product status-publish first instock shipping-taxable purchasable product-type-simple">
+									<div className="wc-block-components-product-image wc-block-grid__product-image wc-block-components-product-image--aspect-ratio-auto wp-block-woocommerce-product-image">
+										<a
+											key={product.id}
+											href={product.permalink}
+											className="product-card"
+										>
+											{product.images?.[0]?.src ? (
+												<img
+													src={product.images[0].src}
+													alt={product.name}
+												/>
+											) : (
+												<img src="http://localhost:8889/wp-content/uploads/woocommerce-placeholder.webp" />
+											)}
+										</a>
 
-									{/* product title */}
-									<h2 className="has-text-align-center wp-block-post-title has-medium-font-size"><a href="">{product.name} </a></h2>
+										{/* product title */}
+										<h2 className="has-text-align-center wp-block-post-title has-medium-font-size"><a href="">{product.name} </a></h2>
 
-									{/* price */}
-									<div className="has-font-size has-small-font-size has-text-align-center wp-block-woocommerce-product-price has-small-font-size">
-										<div className="wc-block-components-product-price wc-block-grid__product-price">
-											<span className="woocommerce-Price-amount amount">$299(p)</span>
+										{/* price */}
+										<div className="has-font-size has-small-font-size has-text-align-center wp-block-woocommerce-product-price has-small-font-size">
+											<div className="wc-block-components-product-price wc-block-grid__product-price">
+												<span className="woocommerce-Price-amount amount">$299(pkoro)</span>
+											</div>
+										</div>
+
+										{/* add to cart btn */}
+										{/* class dynamic */}
+										<div className="wp-block-button wc-block-components-product-button   align-center wp-block-woocommerce-product-button has-small-font-size">
+											<a href="" className="wp-block-button__link wp-element-button wc-block-components-product-button__button product_type_simple has-font-size has-small-font-size has-text-align-center wc-interactive">
+												Add to cart (pkoro)
+											</a>
 										</div>
 									</div>
+								</li>
+							))}
+						</ul>
+					</div>
 
-									{/* add to cart btn */}
-									{/* class dynamic */}
-									<div className="wp-block-button wc-block-components-product-button   align-center wp-block-woocommerce-product-button has-small-font-size">
-										<a href="" className="wp-block-button__link wp-element-button wc-block-components-product-button__button product_type_simple has-font-size has-small-font-size has-text-align-center wc-interactive">
-											Add to cart (p)
-										</a>
-									</div>
-								</div>
-							</li>
-						))}
-					</ul>
+					{/* Store Tab Content */}
+					<div className="panel entry-content wc-tab" id="tab-store" style={{ display: activeTab === 'store' ? 'block' : 'none' }}>
+						<div className="store-info">
+							<h3>Store Information</h3>
+						</div>
+					</div>
 				</div>
 			</div>
-			
+
 		</div>
 	);
 };
