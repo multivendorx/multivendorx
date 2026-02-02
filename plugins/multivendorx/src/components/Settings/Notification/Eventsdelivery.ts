bd54c12b5ef871de13519d6e27b6e3ccf3c7874a
07sagarsaha <ss2749968@gmail.com>
2026-02-02 20:47:59 +0530

    conflict

--- plugins/multivendorx/src/components/Settings/Notification/Eventsdelivery.ts
+++ plugins/multivendorx/src/components/Settings/Notification/Eventsdelivery.ts
@@ -1,5 +1,4 @@
 import { __ } from '@wordpress/i18n';
-import { CountryCodes } from 'zyra';
 
 export default {
 	id: 'notification-configuration',
@@ -36,9 +35,13 @@ export default {
 			),
 			preText: {
 				type: 'select',
-				key: 'country_code',
+				key: 'store_base',
 				size: '90px',
-				options: CountryCodes,
+				options: [
+					{ label: '+91', value: 'store' },
+					{ label: '+92', value: 'shop' },
+					{ label: '+93', value: 'vendor' },
+				],
 			},
 		},
 		{
@@ -89,12 +92,6 @@ export default {
 			type: 'text',
 			label: __('Sender phone number', 'multivendorx'),
 			placeholder: __('+1 4151234567', 'multivendorx'),
-			preText: {
-				type: 'select',
-				key: 'country_code',
-				size: '90px',
-				options: CountryCodes,
-			},
 			settingDescription: __(
 				'The phone number SMS messages are sent from. Use country code followed by phone number. Example: +1 – 4151234567, +91 – 9876543210',
 				'multivendorx'
@@ -220,6 +217,5 @@ export default {
 				value: 'plivo',
 			},
 		},
-		
 	],
 };
