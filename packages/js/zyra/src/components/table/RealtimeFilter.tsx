4659ec11c907f4c9116807bb9c88732aeeb155d6
07sagarsaha <ss2749968@gmail.com>
2026-02-14 19:05:35 +0530

    Update RealtimeFilter.tsx

--- packages/js/zyra/src/components/table/RealtimeFilter.tsx
+++ packages/js/zyra/src/components/table/RealtimeFilter.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect } from 'react';
 import { RealtimeFilterConfig, TableRow } from './types';
 import { SelectInputUI } from '../SelectInput';
 import { AdminButtonUI } from '../AdminButton';
