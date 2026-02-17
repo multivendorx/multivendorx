df2b5004272595c8f3dfb286986c34a5be4ff7a5
Amit Sarkar <88242426+imroxygen@users.noreply.github.com>
2026-02-17 15:36:16 +0530

    elemnt widgets

--- plugins/multivendorx/modules/Elementor/widgets/StoreDescription.php
+++ plugins/multivendorx/modules/Elementor/widgets/StoreDescription.php
@@ -1,63 +1,72 @@
 <?php
+
 namespace MultiVendorX\Elementor\Widgets;
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-use Elementor\Widget_Base;
+use Elementor\Widget_Heading;
 use Elementor\Controls_Manager;
 use MultiVendorX\Elementor\StoreHelper;
 
-class Store_Description extends Widget_Base {
+class Store_Description extends Widget_Heading{
 
 	use StoreHelper;
 
-	public function get_name() {
+	public function get_name()
+	{
 		return 'multivendorx_store_description';
 	}
 
-	public function get_title() {
-		return __( 'Store Description', 'multivendorx' );
+	public function get_title()
+	{
+		return __('Store Description', 'multivendorx');
 	}
 
-	public function get_icon() {
+	public function get_icon()
+	{
 		return 'eicon-editor-paragraph';
 	}
 
-	public function get_categories() {
-		return array( 'multivendorx' );
+	public function get_categories()
+	{
+		return array('multivendorx');
 	}
 
-	protected function register_controls() {
-		$this->start_controls_section(
-			'content',
-			array(
-				'label' => __( 'Content', 'multivendorx' ),
-			)
+	protected function register_controls(){
+		parent::register_controls();
+
+		$this->update_control(
+			'title',
+			[
+				'label'   => __('Default Description', 'multivendorx'),
+				'type'    => Controls_Manager::TEXTAREA,
+				'default' => __('No store description available.', 'multivendorx'),
+				'dynamic' => [
+					'active' => true,
+				],
+			]
 		);
 
-		$this->add_control(
-			'empty_text',
-			array(
-				'label'   => __( 'Empty Description Text', 'multivendorx' ),
-				'type'    => Controls_Manager::TEXT,
-				'default' => __( 'No store description available.', 'multivendorx' ),
-			)
+		$this->update_control(
+			'header_size',
+			[
+				'default' => 'p', 
+			]
 		);
 
-		$this->end_controls_section();
+		$this->remove_control('link');
 	}
 
 	protected function render() {
-
 		$store = $this->get_store_data();
-		if ( ! $store ) {
+		if (! $store) {
 			return;
-        }
+		}
 
 		$description = $store['storeDescription'] ?: $this->get_settings_for_display()['empty_text'];
 
-		echo '<div class="mvx-store-description">';
-		echo wp_kses_post( wpautop( $description ) );
+		echo '<div class="multivendorx-store-description">';
+		echo wp_kses_post(wpautop($description));
 		echo '</div>';
 	}
 }
