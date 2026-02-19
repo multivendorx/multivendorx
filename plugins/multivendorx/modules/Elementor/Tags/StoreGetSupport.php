49a5161c0918ac076a792d3f75d65b20250c19dd
user-promi <promita@dualcube.com>
2026-02-19 12:23:10 +0530

    elementor tag add

---
+++ plugins/multivendorx/modules/Elementor/Tags/StoreGetSupport.php
@@ -0,0 +1,64 @@
+<?php
+namespace MultiVendorX\Elementor\Tags;
+use Elementor\Core\DynamicTags\Tag;
+use Elementor\Modules\DynamicTags\Module;
+
+class StoreGetSupport extends Tag {
+
+    /**
+     * Class constructor
+     *
+     * @since 3.7
+     *
+     * @param array $data
+     */
+    public function __construct( $data = [] ) {
+        parent::__construct( $data );
+    }
+
+    /**
+     * Tag name
+     *
+     * @since 3.7
+     *
+     * @return string
+     */
+    public function get_name() {
+        return 'mvx-store-get-support-tag';
+    }
+
+    public function get_group() {
+        return 'multivendorx'; // must match group ID
+    }
+
+    public function get_categories() {
+        return [ Module::TEXT_CATEGORY ];
+    }
+
+    /**
+     * Tag title
+     *
+     * @since 3.7
+     *
+     * @return string
+     */
+    public function get_title() {
+        return __('Store Get Support Button', 'multivendorx');
+    }
+
+    /**
+     * Render tag
+     *
+     * @since 3.7
+     *
+     * @return void
+     */
+    public function render() {
+    	global $MVX;
+        // if (mvx_is_store_page()) {
+        	echo esc_html_e('Get Support', 'multivendorx');
+        // } else {
+        //     return;
+        // }
+    }
+}
---
+++ plugins/multivendorx/modules/Elementor/Tags/StoreName.php
@@ -0,0 +1,28 @@
+<?php
+namespace MultiVendorX\Elementor\Tags;
+
+use Elementor\Core\DynamicTags\Tag;
+use Elementor\Modules\DynamicTags\Module;
+
+class StoreName extends Tag {
+
+    public function get_name() {
+        return 'mvx-store-name';
+    }
+
+    public function get_title() {
+        return __( 'Store Name', 'multivendorx' );
+    }
+
+    public function get_group() {
+        return 'multivendorx'; // must match group ID
+    }
+
+    public function get_categories() {
+        return [ Module::TEXT_CATEGORY ];
+    }
+
+    public function render() {
+        echo 'storeeee';
+    }
+}
