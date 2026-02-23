20a647499c543e86599e4e261cde79cce31ae27d
user-promi <promita@dualcube.com>
2026-02-23 19:01:30 +0530

    elementor tag folder full dynamic and widgets half

--- plugins/multivendorx/modules/Elementor/Tags/StoreSocial.php
+++ plugins/multivendorx/modules/Elementor/Tags/StoreSocial.php
@@ -1,10 +1,11 @@
 <?php
 namespace MultiVendorX\Elementor\Tags;
-
 use Elementor\Core\DynamicTags\Tag;
 use Elementor\Modules\DynamicTags\Module;
+use MultiVendorX\Elementor\StoreHelper;
 
 class StoreSocial extends Tag {
+    use StoreHelper;
 
     /**
      * Class constructor
@@ -13,7 +14,7 @@ class StoreSocial extends Tag {
      *
      * @param array $data
      */
-    public function __construct( $data = array() ) {
+    public function __construct( $data = [] ) {
         parent::__construct( $data );
     }
 
@@ -25,7 +26,7 @@ class StoreSocial extends Tag {
      * @return string
      */
     public function get_name() {
-        return 'mvx-store-social-tag';
+        return 'multivendorx-store-social-tag';
     }
 
     /**
@@ -44,7 +45,7 @@ class StoreSocial extends Tag {
     }
 
     public function get_categories() {
-        return array( Module::TEXT_CATEGORY );
+        return [ Module::TEXT_CATEGORY ];
     }
 
     /**
@@ -55,55 +56,39 @@ class StoreSocial extends Tag {
      * @return void
      */
     public function render() {
-        // global $MVX, $mvx_elementor;
-        // $links       = [];
-        // $network_map = $mvx_elementor->get_social_networks_map();
+        $links       = [];
+        $network_map = [
+                'fb'        => 'fab fa-facebook',
+                'gplus'     => 'fab fa-google-plus',
+                'twitter'   => 'fab fa-twitter',
+                'linkedin'  => 'fab fa-linkedin',
+                'youtube'   => 'fab fa-youtube',
+                'instagram' => 'fab fa-instagram',
+                'pinterest'	=> 'fab fa-pinterest'
+			];
 
-        // if ( mvx_is_store_page() ) {
-        // $store_id = mvx_find_shop_page_vendor();
+     
+        $social_info = $this->get_social_profiles();
 
-        // $social_info = $this->get_social_profiles($store_id);
+        foreach ( $network_map as $mvx_name => $elementor_name ) {
+            if ( ! empty( $social_info[ $mvx_name ] ) ) {
+                $links[ $elementor_name ] = $social_info[ $mvx_name ];
+            }
+        }
 
-        // foreach ( $network_map as $mvx_name => $elementor_name ) {
-        // if ( ! empty( $social_info[ $mvx_name ] ) ) {
-        // $links[ $elementor_name ] = $social_info[ $mvx_name ];
-        // }
-        // }
-        // } else {
-        // foreach ( $network_map as $mvx_name => $elementor_name ) {
-        // $links[ $elementor_name ] = '#';
-        // }
-        // }
-
-        echo '#';
+        echo $links;
     }
 
-    protected function get_social_profiles( $vendor_id ) {
-        $store_infos              = array();
-        $vendor_fb_profile        = get_user_meta( $vendor_id, '_vendor_fb_profile', true );
-        $vendor_twitter_profile   = get_user_meta( $vendor_id, '_vendor_twitter_profile', true );
-        $vendor_linkdin_profile   = get_user_meta( $vendor_id, '_vendor_linkdin_profile', true );
-        $vendor_youtube           = get_user_meta( $vendor_id, '_vendor_youtube', true );
-        $vendor_instagram         = get_user_meta( $vendor_id, '_vendor_instagram', true );
-        $vendor_pinterest_profile = get_user_meta( $vendor_id, '_vendor_pinterest_profile', true );
-        if ( $vendor_fb_profile ) {
-			$store_infos['fb'] = $vendor_fb_profile;
-        }
-        if ( $vendor_twitter_profile ) {
-			$store_infos['twitter'] = $vendor_twitter_profile;
-        }
-        if ( $vendor_linkdin_profile ) {
-			$store_infos['linkedin'] = $vendor_linkdin_profile;
-        }
-        if ( $vendor_youtube ) {
-			$store_infos['youtube'] = $vendor_youtube;
-        }
-        if ( $vendor_instagram ) {
-			$store_infos['instagram'] = $vendor_instagram;
-        }
-        if ( $vendor_pinterest_profile ) {
-			$store_infos['pinterest'] = $vendor_pinterest_profile;
-        }
+    protected function get_social_profiles() {
+        $store = $this->get_store_data();
+        $store_infos = array();
+        $store_infos['fb'] = $store['facebook'];  
+        $store_infos['twitter'] = $store['twitter'];
+        $store_infos['linkedin'] = $store['linkedin'];
+        $store_infos['youtube'] = $store['youtube'];
+        $store_infos['instagram'] = $store['instagram'];
+        $store_infos['pinterest'] = $store['pinterest'];
         return $store_infos;
     }
+
 }
