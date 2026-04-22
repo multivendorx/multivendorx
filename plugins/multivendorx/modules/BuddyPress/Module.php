8f3d0a99fc56a46815ca4f1758f5cb226ec6a643
divineshiva003 <raishivam.in2k3@gmail.com>
2026-04-22 19:03:36 +0530

    edit

--- plugins/multivendorx/modules/BuddyPress/Module.php
+++ plugins/multivendorx/modules/BuddyPress/Module.php
@@ -43,7 +43,7 @@ class Module {
      * @return void
      */
     public function init_classes() {
-        $this->container['frontend'] = new Frontend();
+        $this->container['functions'] = new Functions();
     }
 
     /**
