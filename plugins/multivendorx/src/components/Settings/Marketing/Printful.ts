7dce363e547041f66cc4fc9314394fd7405f3ac8
divineshiva003 <raishivam.in2k3@gmail.com>
2026-04-24 21:24:37 +0530

    printful

---
+++ plugins/multivendorx/src/components/Settings/Marketing/Printful.ts
@@ -0,0 +1,27 @@
+/* global appLocalizer */
+import { __ } from '@wordpress/i18n';
+
+export default {
+    id: 'printful',
+    priority: 7,
+    headerTitle: __('Printful', 'multivendorx'),
+    settingTitle: 'Printful settings',
+    headerDescription: __(
+        'Set purchase limits for individual products to prevent bulk buying or ensure minimum order quantities.',
+        'multivendorx'
+    ),
+    headerIcon: 'printful',
+    submitUrl: 'settings',
+    modal: [
+        {
+            key: 'client-id',
+            type: 'text',
+            label: __('Client ID','multivendorx'),
+        },
+        {
+            key: 'secret-key',
+            type: 'text',
+            label: __('Secret Key','multivendorx'),
+        }
+    ],
+};
