87721b66464a59580d64a1ac6090e655b3e111bb
moumitahalder <moumita.h@dualcube.com>
2026-04-27 12:30:32 +0530

    Printfull settings

--- plugins/multivendorx/src/components/Settings/Marketing/Printful.ts
+++ plugins/multivendorx/src/components/Settings/Marketing/Printful.ts
@@ -6,7 +6,7 @@ export default {
     headerTitle: __('Printful', 'multivendorx'),
     settingTitle: 'Printful settings',
     headerDescription: __(
-        'Configure your Printful integration by entering your Client ID and Secret Key to connect your store.',
+        'Connect your store with Printful to automate product sync, order fulfillment, and inventory updates.',
         'multivendorx'
     ),
     headerIcon: 'printful',
@@ -16,11 +16,17 @@ export default {
             key: 'client-id',
             type: 'text',
             label: __('Client ID','multivendorx'),
+            settingDescription : __( 'Connect Printful at admin level to allow stores to access and sell Printful products.', 'multivendorx' ),
+            desc: __( 'Used to identify your application when connecting with Printful.<br/>Get your API credentials from the Printful Developer settings.<br/><a href="https://developers.printful.com/docs/#section/Authentication" class="link-item" target="_blank">Printful Authentication guide <i class="adminfont-external"></i></a>', 'multivendorx' ),
+
         },
         {
             key: 'secret-key',
             type: 'password',
             label: __('Secret Key','multivendorx'),
+            settingDescription: __( 'Secure key used to authenticate Printful connection for all stores.', 'multivendorx' ),
+            desc: __( 'Keep this key private. It is required to process product sync, orders, and fulfillment via Printful.', 'multivendorx' ),
+
         }
     ],
 };
