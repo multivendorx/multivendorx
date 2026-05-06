e417509568b4ad314b62f6e83bd6fc96835a6df5
buddhadebroy <roybuddhadeb2003@gmail.com>
2026-05-06 19:14:52 +0530

    css fixed

--- plugins/multivendorx/templates/store/store-dashboard.php
+++ plugins/multivendorx/templates/store/store-dashboard.php
@@ -14,6 +14,7 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
