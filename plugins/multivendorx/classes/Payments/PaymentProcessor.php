f6fb5257cd1a5c7b0b36a8e94b6cb794bfb5f037
user-promi <promita@dualcube.com>
2026-05-25 23:00:01 +0530

    Update PaymentProcessor.php

--- plugins/multivendorx/classes/Payments/PaymentProcessor.php
+++ plugins/multivendorx/classes/Payments/PaymentProcessor.php
@@ -151,7 +151,7 @@ class PaymentProcessor {
 		global $wpdb;
 
         if ( $additional_receiver > 0 ) {
-			do_action( 'multivendorx_handle_user_payment', $store_id, $order_id, $transaction_id, $amount );
+			do_action( 'multivendorx_handle_user_payment', $store_id, $order_id, $transaction_id, $amount, $additional_receiver );
 			return;
 		}
 
