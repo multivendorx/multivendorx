2f00044a412f3e5d06682ae5393777c82798f40d
buddhadebroy <roybuddhadeb2003@gmail.com>
2026-06-09 19:52:07 +0530

    code add

--- plugins/catalogx/classes/Frontend.php
+++ plugins/catalogx/classes/Frontend.php
@@ -32,7 +32,7 @@ class Frontend {
      */
     public function enqueue_frontend_assets() {
         FrontendScripts::enqueue_frontend_assets();
-        // css load in quote page (didi) 
+        // add quote table css (didi)
         // if ( is_product() || is_shop() || is_account_page() ) {
             FrontendScripts::enqueue_style( 'catalogx-frontend-style' );
         // }
