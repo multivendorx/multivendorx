7a91887724d0c75171276fe1fb12f0f50b0df8ce
User-promi <promita@dualcube.com>
2026-06-29 18:01:57 +0530

    change log

--- plugins/multivendorx/dc_product_vendor.php
+++ plugins/multivendorx/dc_product_vendor.php
@@ -4,12 +4,12 @@
  * Plugin URI: https://multivendorx.com/
  * Description: An AI-powered WooCommerce multivendor marketplace solution to build, manage, and scale your platform.
  * Author: MultiVendorX
- * Version: 5.0.8
+ * Version: 5.0.9
  * Author URI: https://multivendorx.com/
  * Requires at least: 6.3
  * Tested up to: 7.0.0
  * WC requires at least: 8.2.0
- * WC tested up to: 10.8.1
+ * WC tested up to: 10.9.1
  *
  * Text Domain: multivendorx
  * Requires Plugins: woocommerce
