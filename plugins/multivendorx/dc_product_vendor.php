b7e169e6cdd22b4bd8249470a6d917aded843b79
Amit <myoxygen2000@gmail.com>
2026-07-23 18:27:40 +0530

    fix

--- plugins/multivendorx/dc_product_vendor.php
+++ plugins/multivendorx/dc_product_vendor.php
@@ -4,10 +4,10 @@
  * Plugin URI: https://multivendorx.com/?utm_source=wpadmin&utm_medium=pluginsettings&utm_campaign=multivendorx
  * Description: An AI-powered WooCommerce multivendor marketplace solution to build, manage, and scale your platform.
  * Author: MultiVendorX
- * Version: 5.0.10
+ * Version: 5.0.11
  * Author URI: https://multivendorx.com/?utm_source=wpadmin&utm_medium=pluginsettings&utm_campaign=multivendorx
  * Requires at least: 6.3
- * Tested up to: 7.0.1
+ * Tested up to: 7.0.2
  * WC requires at least: 8.2.0
  * WC tested up to: 10.9.4
  *
