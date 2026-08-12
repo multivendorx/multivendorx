c7b21cbad6672b2717e50073d2da9211ca160d89
Amit <myoxygen2000@gmail.com>
2026-08-12 17:17:38 +0530

    fix

--- plugins/multivendorx/classes/RestAPI/Controllers/Stores.php
+++ plugins/multivendorx/classes/RestAPI/Controllers/Stores.php
@@ -847,7 +847,7 @@ class Stores extends \WP_REST_Controller {
                 'primary_owner_info' => $primary_owner_info,
                 'overall_reviews'    => $overall_reviews,
                 'total_reviews'      => is_array( $reviews ) ? count( $reviews ) : 0,
-                'payment_method'     => $store->get_payment_method('name')
+                'payment_method'     => $store->get_payment_method( 'name' ) ?: '',
             );
 
             foreach ( (array) $store->meta_data as $key => $values ) {
