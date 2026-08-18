31d5a31386099109b15d143768912a1f7124f043
Amit <myoxygen2000@gmail.com>
2026-08-18 16:36:34 +0530

    fix

--- plugins/catalogx/modules/Quote/QuoteCart.php
+++ plugins/catalogx/modules/Quote/QuoteCart.php
@@ -87,7 +87,7 @@ class QuoteCart extends \WP_REST_Controller {
         }
 
         // Check if user is admin or customer for other authenticated requests.
-        return current_user_can( 'customer' ) || current_user_can( 'manage_options' );
+        return current_user_can( 'customer' ) || current_user_can( 'wholesale_user' ) || current_user_can( 'manage_options' );
     }
 
     /**
