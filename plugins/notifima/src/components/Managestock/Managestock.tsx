24782bf264895727f7c53201ad4170da8cdc53cb
buddhadebroy <roybuddhadeb2003@gmail.com>
2026-09-24 19:27:15 +0530

    notifima

--- plugins/notifima/src/components/Managestock/Managestock.tsx
+++ plugins/notifima/src/components/Managestock/Managestock.tsx
@@ -120,8 +120,8 @@ const Managestock = () => {
         categoryCounts: defaultCategoryCounts,
         filters,
         expandable: true,
-        expandText: 'View variations',
-        expandedText: 'Viewing',
+        expandText: __('View variations', 'notifima'),
+        expandedText: __('Viewing', 'notifima'),
         search: {
             placeholder: __('Search...', 'notifima'),
             size: 8,
